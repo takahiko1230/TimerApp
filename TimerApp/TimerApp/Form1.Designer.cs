56fa22cf5d7f9c2c33d6fffd59e0e71a7f223f89
takahiko1230 <71083601+takahiko1230@users.noreply.github.com>
2020-10-13 00:06:30 +0900

    ChangeStart
    ChangeStart

--- TimerApp/TimerApp/Form1.Designer.cs
+++ TimerApp/TimerApp/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DisplayTime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radio30 = new System.Windows.Forms.RadioButton();
+            this.radio60 = new System.Windows.Forms.RadioButton();
+            this.radio120 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // DisplayTime
+            // 
+            this.DisplayTime.AutoSize = true;
+            this.DisplayTime.Font = new System.Drawing.Font("MS UI Gothic", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.DisplayTime.Location = new System.Drawing.Point(43, 50);
+            this.DisplayTime.Name = "DisplayTime";
+            this.DisplayTime.Size = new System.Drawing.Size(583, 107);
+            this.DisplayTime.TabIndex = 0;
+            this.DisplayTime.Text = "DisplayTime";
+            this.DisplayTime.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "タイマー開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radio30
+            // 
+            this.radio30.AutoSize = true;
+            this.radio30.Location = new System.Drawing.Point(61, 226);
+            this.radio30.Name = "radio30";
+            this.radio30.Size = new System.Drawing.Size(89, 28);
+            this.radio30.TabIndex = 5;
+            this.radio30.TabStop = true;
+            this.radio30.Text = "30分";
+            this.radio30.UseVisualStyleBackColor = true;
+            // 
+            // radio60
+            // 
+            this.radio60.AutoSize = true;
+            this.radio60.Location = new System.Drawing.Point(61, 281);
+            this.radio60.Name = "radio60";
+            this.radio60.Size = new System.Drawing.Size(89, 28);
+            this.radio60.TabIndex = 6;
+            this.radio60.TabStop = true;
+            this.radio60.Text = "60分";
+            this.radio60.UseVisualStyleBackColor = true;
+            // 
+            // radio120
+            // 
+            this.radio120.AutoSize = true;
+            this.radio120.Location = new System.Drawing.Point(61, 338);
+            this.radio120.Name = "radio120";
+            this.radio120.Size = new System.Drawing.Size(101, 28);
+            this.radio120.TabIndex = 7;
+            this.radio120.TabStop = true;
+            this.radio120.Text = "120分";
+            this.radio120.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.radio120);
+            this.Controls.Add(this.radio60);
+            this.Controls.Add(this.radio30);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DisplayTime);
+            this.Name = "Form1";
+            this.Text = "タイマー";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label DisplayTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radio30;
+        private System.Windows.Forms.RadioButton radio60;
+        private System.Windows.Forms.RadioButton radio120;
     }
 }
 
--- TimerApp/TimerApp/Form1.cs
+++ TimerApp/TimerApp/Form1.cs
@@ -1,20 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace TimerApp
 {
     public partial class Form1 : Form
     {
+        public static System.Threading.Timer timer=null;
+        DateTime dateTime;
+        int second;
+
+        ManualResetEvent mre = new ManualResetEvent(false);
+
         public Form1()
         {
             InitializeComponent();
+            TimeStart();
+        }
+        
+        public void TimeStart()
+        {
+            TimerCallback timerDelegate = new TimerCallback(OnTimeCallback);
+
+            // タイマーを生成し、開始します。
+            // 例では1000ミリ秒=1秒間隔で処理を呼び出します。
+            timer = new System.Threading.Timer(timerDelegate, null, 0, 1000);
+
+        }
+
+        public void OnTimeCallback(object obj)
+        {
+            dateTime = DateTime.Now;
+            DisplayTime.Text = String.Format("{0}時{1}分です。", dateTime.Hour,dateTime.Minute);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //チェックボックスごとの計測
+            if (radio30.Checked)
+            {
+                StartTimer(30);
+            }
+            if(radio60.Checked)
+            {
+                StartTimer(60);
+            }
+            if(radio120.Checked)
+            {
+                StartTimer(120);
+            }
+        }
+
+        public void StartTimer(int resTime)
+        {
+            System.Threading.Timer thrTime = null;
+            //タイマー終了時に呼び出す関数
+            TimerCallback resDelegate = new TimerCallback(ResCallBack);
+
+            int timerTime = resTime * 60000;
+            thrTime = new System.Threading.Timer(resDelegate, null, timerTime, timerTime);
+        }
+
+        public void ResCallBack(object obj) 
+        {
+            MessageBox.Show("タイマー", "タイマーが終了しました", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
         }
     }
 }
+
+            
